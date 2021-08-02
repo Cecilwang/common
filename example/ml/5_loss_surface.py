328fd7b0c803765428b53ecd7235b3e5f793e60e
cecilwang <cecilwang95@gmail.com>
2021-08-02 19:31:01 +0900

    Trajectory

--- example/ml/5_loss_surface.py
+++ example/ml/5_loss_surface.py
@@ -2,13 +2,10 @@ import argparse
 import copy
 from itertools import cycle
 
-import loss_landscapes
-from loss_landscapes.model_interface.model_wrapper import wrap_model
-from loss_landscapes.model_interface.model_parameters import rand_u_like
-from loss_landscapes.model_interface.model_parameters import orthogonal_to
-import matplotlib
 import matplotlib.pyplot as plt
 import numpy as np
+from sklearn.decomposition import PCA
+import torch
 
 from python import util
 
@@ -23,11 +20,11 @@ def parse_args():
     parser.add_argument("sfc", type=str)
     parser.add_argument("traj", type=str)
     parser.add_argument("--e", type=int, default=10)
-    parser.add_argument("--batch_size", type=int, default=4096)
+    parser.add_argument("--batch_size", type=int, default=128)
     parser.add_argument("--data", type=str, default="./.data")
     parser.add_argument("--log", type=str, default="./.log")
-    parser.add_argument("--dist", type=float, default=10)
-    parser.add_argument("--step", type=int, default=50)
+    parser.add_argument("--device", type=str, default="cpu")
+    parser.add_argument("--step", type=int, default=5)
     return parser.parse_args()
 
 
@@ -36,121 +33,165 @@ def random_color(i, n, name='hsv'):
     return next(Color)
 
 
-def sfcfile(dir, sfc, i, j):
-    return "{}/{}-{}-{}.pkl".format(dir, sfc, i, j)
+def flatten_parameters(params):
+    return torch.cat([
+        p.view(p.numel()) if p.dim() > 1 else torch.FloatTensor(p)
+        for p in params
+    ])
 
 
-# Modified from https://github.com/marcellodebernardi/loss-landscapes/blob/8d3461045f317bc0f4ba35e552fb22f3242647ff/loss_landscapes/main.py
-def random_plane(model,
-                 metric,
-                 distance,
-                 steps,
-                 normalization,
-                 dir,
-                 sfc,
-                 deepcopy_model=False):
-
-    model_start_wrapper = wrap_model(
-        copy.deepcopy(model) if deepcopy_model else model)
-
-    start_point = model_start_wrapper.get_module_parameters()
-    dir_one = rand_u_like(start_point)
-    dir_two = orthogonal_to(dir_one)
-
-    if normalization == 'model':
-        dir_one.model_normalize_(start_point)
-        dir_two.model_normalize_(start_point)
-    elif normalization == 'layer':
-        dir_one.layer_normalize_(start_point)
-        dir_two.layer_normalize_(start_point)
-    elif normalization == 'filter':
-        dir_one.filter_normalize_(start_point)
-        dir_two.filter_normalize_(start_point)
-    elif normalization is None:
-        pass
-    else:
-        raise AttributeError('Unsupported normalization argument.')
-
-    dir_one.mul_(
-        ((start_point.model_norm() / distance) / steps) / dir_one.model_norm())
-    dir_two.mul_(
-        ((start_point.model_norm() / distance) / steps) / dir_two.model_norm())
-    dir_one.mul_(steps / 2)
-    dir_two.mul_(steps / 2)
-    start_point.sub_(dir_one)
-    start_point.sub_(dir_two)
-    dir_one.truediv_(steps / 2)
-    dir_two.truediv_(steps / 2)
-
-    data_matrix = []
-    for i in range(steps):
-        data_column = []
-
-        if i % 2 == 0:
-            for j in range(steps):
-                start_point.add_(dir_two)
-                loss = metric(model_start_wrapper)
-                data_column.append(loss)
-                util.save((loss, start_point), sfcfile(dir, sfc, i, j))
-        else:
-            for j in range(steps)[::-1]:
-                start_point.sub_(dir_two)
-                loss = metric(model_start_wrapper)
-                data_column.insert(0, loss)
-                util.save((loss, start_point), sfcfile(dir, sfc, i, j))
-
-        data_matrix.append(data_column)
-        start_point.add_(dir_one)
-
-    return np.array(data_matrix)
+def direction2parameters(direction, params_like):
+    params = copy.deepcopy(params_like)
+    i = 0
+    for p in params:
+        p.copy_(torch.tensor(direction[i:i + p.numel()]).view(p.size()))
+        i += p.numel()
+    assert i == len(direction)
+    return params
 
 
-def draw_surface(args, sfc, ax):
-    model, loader, _, loss_fn = MNIST(**vars(args))
-    M.load(model, args.log + "/{}-{}.pkl".format(sfc, args.e))
-
-    x, y = iter(loader).__next__()
-    loss_fn = loss_landscapes.metrics.Loss(loss_fn, x, y)
-    loss = random_plane(model, loss_fn, args.dist, args.step, "filter",
-                        args.log, sfc)
-
-    X = np.array([[j for j in range(args.step)] for i in range(args.step)])
-    Y = np.array([[i for _ in range(args.step)] for i in range(args.step)])
-    ax.plot_surface(X, Y, loss, alpha=0.8)
+def angle(x, y):
+    return np.dot(x, y) / (np.linalg.norm(x) * np.linalg.norm(y))
 
 
-def draw_trajectory(args, sfc, traj, ax, color="red"):
-    model, _, _, _ = MNIST(**vars(args))
-    xline, yline, zline = [], [], []
-    for e in range(args.e + 1):
-        M.load(model, args.log + "/{}-{}.pkl".format(traj, e))
-        mdiff, mi, mj, mloss = np.finfo(np.float32).max, None, None, None
-        for i in range(args.step):
-            for j in range(args.step):
-                (loss, p) = util.load(sfcfile(args.log, sfc, i, j))
-                diff = 0
-                for x, y in zip(model.parameters(), p.parameters):
-                    diff += np.linalg.norm(x.data - y.data)**2
-                diff = np.sqrt(diff)
-                if diff < mdiff:
-                    mdiff, mi, mj, mloss = diff, i, j, loss
-        print("{} {}-{} {} {}".format(e, mi, mj, mloss, mdiff))
-        xline.append(mi)
-        yline.append(mj)
-        zline.append(mloss)
-    ax.plot3D(xline, yline, zline, label=traj, color=color)
+def get_directions(final_model, traj_files):
+    p1 = [p.data for p in final_model.parameters()]
+
+    matrix = []
+    curr_model = copy.deepcopy(final_model)
+    for model_files in traj_files:
+        for f in model_files:
+            M.load(curr_model, f)
+            p2 = [p.data for p in curr_model.parameters()]
+            d = [y - x for x, y in zip(p1, p2)]
+            matrix.append(flatten_parameters(d).numpy())
+
+    pca = PCA(n_components=2)
+    pca.fit(np.array(matrix))
+    d1 = np.array(pca.components_[0])
+    d2 = np.array(pca.components_[1])
+    print("Angle between directions: {}".format(angle(d1, d2)))
+    return d1, d2
 
 
-def main(args):
+def project(final_model, traj_files, d1, d2, loss_fn):
+    p1 = [p.data for p in final_model.parameters()]
+
+    xs, ys, losses = [], [], []
+    curr_model = copy.deepcopy(final_model)
+    for model_files in traj_files:
+        _xs, _ys, _losses = [], [], []
+        for f in model_files:
+            M.load(curr_model, f)
+            p2 = [p.data for p in curr_model.parameters()]
+            d = [y - x for x, y in zip(p1, p2)]
+            d = flatten_parameters(d).numpy()
+
+            x = np.dot(d, d1) / np.linalg.norm(d1)
+            y = np.dot(d, d2) / np.linalg.norm(d2)
+            loss = loss_fn(curr_model)
+            d = np.linalg.norm(d1 * x + d2 * y - d)
+
+            print("{} at({}, {}) {} {}".format(f, x, y, d, loss))
+
+            _xs.append(int(x))
+            _ys.append(int(y))
+            _losses.append(loss)
+        xs.append(_xs)
+        ys.append(_ys)
+        losses.append(_losses)
+
+    return np.array(xs), np.array(ys), np.array(losses)
+
+
+def get_surface(final_model, d1, d2, xs, ys, loss_fn, step):
+    x_max = int(np.max([np.max(xs), 0]) + 1)
+    x_min = int(np.min([np.min(xs), 0]))
+    y_max = int(np.max([np.max(ys), 0]) + 1)
+    y_min = int(np.min([np.min(ys), 0]))
+    print("({}, {}), ({}, {})".format(x_min, y_min, x_max, y_max))
+
+    x_stride = (x_max - x_min) // step
+    y_stride = (y_max - y_min) // step
+
+    X = [i for i in range(x_min, x_max, x_stride)]
+    Y = [i for i in range(y_min, y_max, y_stride)]
+
+    curr_model = copy.deepcopy(final_model)
+    p = [p.data for p in curr_model.parameters()]
+    final_point = flatten_parameters(p).numpy()
+    loss = []
+    for i in X:
+        column = []
+        for j in Y:
+            p = direction2parameters(d1 * i + d2 * j + final_point, p)
+            for x, y in zip(curr_model.parameters(), p):
+                x.data = y
+            column.append(loss_fn(curr_model))
+        loss.append(column)
+    loss = np.array(loss)
+
+    Y, X = np.meshgrid(Y, X)
+    return X, Y, loss
+
+
+def get_traj_files(args):
+    files = []
+    for x in args.traj.split(","):
+        traj = []
+        for i in range(args.e + 1):
+            traj.append("{}/{}-{}.pkl".format(args.log, x, i))
+        files.append(traj)
+    return files
+
+
+def wrap_loss_fn(loss_fn, loader, device):
+
+    def _loss(model):
+        loss = 0.0
+        for i, (input, target) in enumerate(loader):
+            input, target = input.to(device), target.to(device)
+            output = model(input)
+            loss += loss_fn(output, target).item()
+        return loss / i + 1
+
+    return _loss
+
+
+def draw(surface, trajectories):
     fig = plt.figure()
     ax = plt.axes(projection="3d")
     ax.set_title("Loss Surface")
-    draw_surface(args, args.sfc, ax)
+    ax.plot_surface(*surface, alpha=0.8)
+
     traj = args.traj.split(",")
     for i, x in enumerate(traj):
-        draw_trajectory(args, args.sfc, x, ax, random_color(i, len(traj)))
+        ax.plot3D(trajectories[0][i],
+                  trajectories[1][i],
+                  trajectories[2][i],
+                  label=x,
+                  color=random_color(i, len(traj)))
     ax.legend()
+
+    ax.set_xlabel("x")
+    ax.set_ylabel("y")
+    ax.set_zlabel("loss")
+
     plt.show()
+
+
+def main(args):
+    final_model, loader, _, _loss_fn = MNIST(**vars(args))
+    final_model.eval()
+    M.load(final_model, "{}/{}-{}.pkl".format(args.log, args.sfc, args.e))
+    loss_fn = wrap_loss_fn(_loss_fn, loader, args.device)
+
+    traj_files = get_traj_files(args)
+    d1, d2 = get_directions(final_model, traj_files)
+    trajectories = project(final_model, traj_files, d1, d2, loss_fn)
+    surface = get_surface(final_model, d1, d2, xs, ys, loss_fn, args.step)
+
+    draw(surface, trajectories)
 
 
 if __name__ == "__main__":
